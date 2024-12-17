f2cea48d04519d16c4159b746c1e533c5c59584c
Andrew Veness <7mybqj2j@students.codeinstitute.net>
2024-12-17 14:46:27 +0000

    Add about app and made the corresponding changes to settings.py, urls.py, models.py, admin.py, views.py and add about.html templates

--- blog/views.py
+++ blog/views.py
@@ -36,5 +36,6 @@ def post_detail(request, slug):
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post},
+        {"post": post,
+        "coder": "Andy"},  # context
     )
