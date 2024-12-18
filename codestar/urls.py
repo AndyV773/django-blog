3c2f224b572e8feb1168321ce8ba3e994f03f215
Andrew Veness <7mybqj2j@students.codeinstitute.net>
2024-12-18 11:53:17 +0000

    Add allauth authentication and some template styles

--- codestar/urls.py
+++ codestar/urls.py
@@ -22,7 +22,8 @@ from django.urls import path, include
 urlpatterns = [
     #  or path('blog/', blog.views.blog, name='blog')  #  or path('blog/', my_blog, name='blog')
     path("about/", include("about.urls"), name="about-urls"),
-    path('admin/', admin.site.urls),
-    path('summernote/', include('django_summernote.urls')),
+    path("accounts/", include("allauth.urls")),
+    path("admin/", admin.site.urls),
+    path("summernote/", include("django_summernote.urls")),
     path("", include("blog.urls"), name="blog-urls"),
 ]
