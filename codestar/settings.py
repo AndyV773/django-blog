1500488536f4963570215170f20cda524c82b64e
Andrew Veness <7mybqj2j@students.codeinstitute.net>
2024-12-20 09:10:23 +0000

    Add POST functionality for about form

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-andyv773-djangoblog-57pglrgjk4k.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
